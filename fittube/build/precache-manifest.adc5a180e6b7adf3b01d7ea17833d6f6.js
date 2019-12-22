ac84ea7fb01a3ab1620357b1b75ee48422ca9f69
Alexander Ilyin <alexandr.ilyin@gmail.com>
2019-12-22 23:09:24 +0500

    videos

---
+++ fittube/build/precache-manifest.adc5a180e6b7adf3b01d7ea17833d6f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f8bd93f5091ed0f7914f74d688d200",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01de2e64187491278661",
+    "url": "/static/css/main.e66140ce.chunk.css"
+  },
+  {
+    "revision": "7668433a08d3061c2759",
+    "url": "/static/js/2.292f12aa.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.292f12aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01de2e64187491278661",
+    "url": "/static/js/main.c84affaa.chunk.js"
+  },
+  {
+    "revision": "b0499d5514a896747934",
+    "url": "/static/js/runtime-main.4d5fce11.js"
+  },
+  {
+    "revision": "3992e836b16c7d16edcb4bc6a6b6542f",
+    "url": "/static/media/bg.3992e836.png"
+  }
+]);
